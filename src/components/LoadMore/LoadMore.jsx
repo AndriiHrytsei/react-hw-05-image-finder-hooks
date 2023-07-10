ccb2e699814efc1a4a4ce039a499b08c35395b28
Andrii Hrytsei <andrii.hrytsei@gmail.com>
2023-07-10 18:48:45 +0200

    fix

--- src/components/LoadMore/LoadMore.jsx
+++ src/components/LoadMore/LoadMore.jsx
@@ -2,17 +2,6 @@
 import styles from './LoadMore.module.css';
 import React from 'react'
 
-// export default class LoadMore extends Component {
-//   render() {
-//     return (
-//       <button className={styles.Button} type="button" onClick={this.onClick}>
-//         Load more
-//       </button>
-//     )
-//   }
-// }
-
-
 const LoadMore = ({ inreamentFunc }) => (
   <button className={styles.Button} type="button" onClick={inreamentFunc}>
     Load more
